fe7926bd41e26ad496ee241963ead1e98af29a9f
Daniel Bratulic <danielbratulic@yahoo.com>
2018-04-25 12:10:41 +0200

    Added the utils package and constants file

---
+++ nba_stats/utils/constants.py
@@ -0,0 +1,50 @@
+HEADERS = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/57.0.2987.133 Safari/537.36',
+    'Dnt': '1',
+    'Accept-Encoding': 'gzip, deflate, sdch',
+    'Accept-Language': 'en',
+    'origin': 'http://stats.nba.com'
+}
+
+CURRENT_SEASON = "2017-18"
+
+BASE_STATS_URL = "http://stats.nba.com/stats/"
+SHOTCHART_PARAM = "shotchartdetail"
+PLAYERS_PARAM = "commonallplayers"
+
+
+# Thanks to the seemethere's github repository nba_py
+# (https://github.com/seemethere/nba_py/wiki/stats.nba.com-Endpoint-Documentation)
+TEAMS_IDS = {
+    "Atlanta Hawks": 1610612737,
+    "Boston Celtics": 1610612738,
+    "Brooklyn Nets": 1610612751,
+    "Charlotte Hornets": 1610612766,
+    "Chicago Bulls": 1610612741,
+    "Cleveland Cavaliers": 1610612739,
+    "Dallas Mavericks": 1610612742,
+    "Denver Nuggets": 1610612743,
+    "Detroit Pistons": 1610612765,
+    "Golden State Warriors": 1610612744,
+    "Houston Rockets": 1610612745,
+    "Indiana Pacers": 1610612754,
+    "Los Angeles Clippers": 1610612746,
+    "Los Angeles Lakers": 1610612747,
+    "Memphis Grizzlies": 1610612763,
+    "Miami Heat": 1610612748,
+    "Milwaukee Bucks": 1610612749,
+    "Minnesota Timberwolves": 1610612750,
+    "New Orleans Pelicans": 1610612740,
+    "New York Knicks": 1610612752,
+    "Oklahoma City Thunder": 1610612760,
+    "Orlando Magic": 1610612753,
+    "Philadelphia 76ers": 1610612755,
+    "Phoenix Suns": 1610612756,
+    "Portland Trail Blazers": 1610612757,
+    "Sacramento Kings": 1610612758,
+    "San Antonio Spurs": 1610612759,
+    "Toronto Raptors": 1610612761,
+    "Utah Jazz": 1610612762,
+    "Washington Wizards": 1610612764
+}
