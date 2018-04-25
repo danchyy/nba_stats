87ec18fe83213a655de12425cd68a12d4f86e8d6
Daniel Bratulic <danielbratulic@yahoo.com>
2018-04-25 12:31:18 +0200

    Added setup.py script for installation

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+setup(
+    name="nba_stats",
+    version="0.1",
+    packages=find_packages(exclude=['tests']),
+
+    install_requires=['docutils>=0.3', 'pandas>=0.20.3', 'requests'],
+
+    include_package_data=True,
+
+    # metadata for upload to PyPI
+    author="Daniel Bratulic",
+    author_email="danielbratulic@gmail.com",
+    description="Package which is used for retrieving stats by NBA players.",
+    license="MIT",
+    keywords="nba stats analytics sports",
+    url="https://github.com/danchyy/nba_stats",  # project home page, if any
+    project_urls={
+        "Source Code": "https://github.com/danchyy/nba_stats"
+    },
+    zip_safe=False
+)
